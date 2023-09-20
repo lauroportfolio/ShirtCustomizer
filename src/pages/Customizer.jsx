e266e8c9d7895fb9d4c5bfc44e5f8cd237752231
lauroportfolio <lauroportfolio@gmail.com>
2023-09-20 14:23:06 -0300

    third commit

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -57,6 +57,21 @@ const Customizer = () => {
 
     try {
       // call our backend to generate an ai image!
+      setGeneratingImg(true)
+
+      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          prompt,
+        })
+      })
+
+      const data = await response.json()
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`)
     } catch (error) {
       alert(error)
     } finally {
@@ -83,10 +98,12 @@ const Customizer = () => {
 
       case "stylishShirt":
         state.isFullTexture = !activeFilterTab[tabName]
+        break
     
       default:
         state.isFullTexture = true,
         state.isLogoTexture = false
+        break
     }
 
     // after setting the state, activeFilterTab is updated
